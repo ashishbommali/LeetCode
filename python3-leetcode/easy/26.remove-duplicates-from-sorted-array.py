cd9dd7bfd2db1d5cfdba933d40efa6830e172a47
Ashish Bommali <ashishbommali@gmail.com>
2024-06-13 21:58:51 -0500

    problem 26- if not unique modify array and increament count

--- python3-leetcode/easy/26.remove-duplicates-from-sorted-array.py
+++ python3-leetcode/easy/26.remove-duplicates-from-sorted-array.py
@@ -8,6 +8,18 @@
 from typing import List
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        
+        index = 0
+        unique = nums[0]
+        count = 1
+        for i in range(1,len(nums)):
+            if unique == nums[i]:
+                continue
+            else:
+                
+                index += 1
+                unique = nums[i]
+                nums[index] = nums[i]
+                count +=1 
+                
+        return count
 # @lc code=end
-
